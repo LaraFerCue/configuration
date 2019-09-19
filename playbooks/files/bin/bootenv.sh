9c4f2f8f31752b5fad6079f40499312da4c89e25
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-09-19 09:18:18 +0200

    Adding a script to use boot environments with encrypted disks

---
+++ playbooks/files/bin/bootenv.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Handles a boot environment using ZFS on an encrypted disk, which needs a
+# separate ZFS pool to boot.
+#
+# Main problem is that the boot pool is used to boot the kernel, and then
+# boot into the boot environment.
+
+usage() {
+	cat > /dev/stderr <<USAGE
+$(basename "${0}") boot_environment kernel
+
+boot_environment:	the created boot environment requested
+			for activation
+kernel:			The kernel to be used with the boot
+			environment.
+USAGE
+}
+
+boot_environment=
+kernel=
+
+while [ "${1}" ] ; do
+	if [ "${1}" = "-h" ] || [ "${1}" = "--help" ] ; then
+		usage
+		exit 0
+	elif [ -z "${boot_environment}" ] ; then
+		boot_environment="${1}"
+	else
+		kernel="${1}"
+	fi
+	shift
+done
+
+if [ -z "${boot_environment}" ] || [ -z "${kernel}" ] ; then
+	usage
+	exit 1
+fi
+
+sed -i '.bak' 's/^kernel=.*$//g' /boot/loader.conf
+echo "kernel=\"${kernel}\"" >> /boot/loader.conf
+
+beadm activate "${boot_environment}"
